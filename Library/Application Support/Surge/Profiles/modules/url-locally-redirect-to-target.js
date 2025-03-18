f9f2b3a2cddacc686739de50981c35f695330d36
bit.kitchen <git@bit.kitchen>
2025-03-18 08:51:57 +0800

    url-locally-redirect-to-target: add sgmodule

---
+++ Library/Application Support/Surge/Profiles/modules/url-locally-redirect-to-target.js
@@ -0,0 +1,22 @@
+// https://manual.nssurge.com/scripting/common.html
+// https://manual.nssurge.com/scripting/http-request.html
+
+// https://www.o365atp.com/
+// https://github.com/newhouse/url-tracking-stripper/blob/master/assets/js/redirects.js
+
+const paramUrl = new URL($request.url).searchParams.get('url')
+if (paramUrl) {
+  // Redirect to the target URL directly without going through ATP safelinks.
+  const targetUrl = decodeURIComponent(paramUrl)
+  $done({
+    response: {
+      status: 302,
+      headers: {
+        'Location': targetUrl,
+      },
+    },
+  })
+} else {
+  // No url parameter found, keep the request untouched.
+  $done({})
+}
