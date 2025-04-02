26d57f40b68480ea42e88cf4c06e62ef0f800808
bit.kitchen <git@bit.kitchen>
2025-04-02 12:11:36 +0800

    url-locally-redirect-to-target: support multiple url search params

--- Library/Application Support/Surge/Profiles/modules/url-locally-redirect-to-target.js
+++ Library/Application Support/Surge/Profiles/modules/url-locally-redirect-to-target.js
@@ -6,9 +6,9 @@
 // https://www.o365atp.com/
 // https://github.com/newhouse/url-tracking-stripper/blob/master/assets/js/redirects.js
 
-let param = 'url'
+let params = ['url']
 try {
-  param = $argument
+  params = $argument.split(',')
 } catch (e) {
   // Ignore ReferenceError if argument not provided in sgmodule.
 }
@@ -17,7 +17,7 @@ try {
 try {
   $request
 } catch (e) {
-  param = process.argv[2] || 'url'
+  params = process.argv[2] ? process.argv[2].split(',') : ['url']
   $request = {
     url: process.argv[3] || 'https://www.google.com/url?url=https://www.google.com/',
   }
@@ -25,18 +25,23 @@ try {
 }
 
 const url = new URL($request.url)
-const paramUrl = url.searchParams.get(param)
-if (paramUrl) {
-  // Redirect to the target URL directly without going through ATP safelinks.
-  const targetUrl = decodeURIComponent(paramUrl)
-  $done({
-    response: {
-      status: 302,
-      headers: {
-        'Location': targetUrl,
-      },
-    },
-  })
+
+if (params.length) {
+  for (const param of params) {
+    const paramUrl = url.searchParams.get(param)
+    if (paramUrl) {
+      // Redirect to the target URL directly without going through ATP safelinks.
+      const targetUrl = decodeURIComponent(paramUrl)
+      $done({
+        response: {
+          status: 302,
+          headers: {
+            'Location': targetUrl,
+          },
+        },
+      })
+    }
+  }
 } else if (url.pathname.startsWith('/lnk/')) {
   const segments = url.pathname.split('/')
   const targetUrl = atob(segments[segments.length - 1])
