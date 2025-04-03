93a13626e7712b183ab81cb8793a755d15573c9a
bit.kitchen <git@bit.kitchen>
2025-04-03 08:16:12 +0800

    url-locally-redirect-to-target: split two types of redirects

---
+++ Library/Application Support/Surge/Profiles/modules/url-locally-redirect-to-target-via-param.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+// https://manual.nssurge.com/scripting/common.html
+// https://manual.nssurge.com/scripting/http-request.html
+
+// https://www.o365atp.com/
+// https://github.com/newhouse/url-tracking-stripper/blob/master/assets/js/redirects.js
+
+let params = ['url']
+try {
+  params = $argument.split('&')
+} catch (e) {
+  // Ignore ReferenceError if argument not provided in sgmodule.
+}
+
+// Allow debugging using Node.js
+try {
+  $request
+} catch (e) {
+  params = process.argv[2] ? process.argv[2].split('&') : ['url']
+  $request = {
+    url: process.argv[3] || 'https://www.google.com/url?url=https://www.google.com/',
+  }
+  $done = console.error
+}
+
+const url = new URL($request.url)
+
+console.log(`params: ${params}`)
+for (const param of params) {
+  const paramUrl = url.searchParams.get(param)
+  console.log(`param: ${param}, value: ${paramUrl}`)
+
+  if (paramUrl) {
+    // Redirect to the target URL directly without going through ATP safelinks.
+    const targetUrl = decodeURIComponent(paramUrl)
+    $done({
+      response: {
+        status: 302,
+        headers: {
+          'Location': targetUrl,
+        },
+      },
+    })
+  }
+}
